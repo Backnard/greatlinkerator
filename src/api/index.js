856c9ad232f54670428375ff45ef4c781f4a1e1c
Sean Greene <seangreenebrandeis@gmail.com>
2020-06-17 22:42:16 -0400

    server up

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,11 @@
 const express = require('express');
 
-const server = express();
+const apiRouter = express.Router();
 
-const BodyParser = require('body-parser');
-server.use(BodyParser.json());
-
-const searchRouter = require('./SearchResults');
-server.use('/SearchResults',searchRouter);
-
-// server.use('/', (req, res, next)=>{
-//     res.send('Welcome to /api!');
-//     next();
-// })
+apiRouter.use('/', (req, res, next)=>{
+    res.send('Entering /api successfully');
+})
 
 
-
+module.exports= apiRouter;
 
