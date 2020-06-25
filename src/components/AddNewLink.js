f069d2d3e6101d3c010f2ee0c0537507e398698f
Patrick-Vincent Herrera <patrick-vincentherrera@Patrick-Vincents-MacBook-Air.local>
2020-06-25 13:51:53 -0700

    Updated with addLINK

---
+++ src/components/AddNewLink.js
@@ -0,0 +1,27 @@
+import React, {} from 'react'
+
+
+const addNewLink = () =>{
+  const handleSubmit = (event) => {
+      const linkFormData = new FormData(event.target);
+      event.preventDefault();
+      for (let [key, value] of linkFormData.entries()) {
+          console.log(key, value);
+      }
+  }
+
+  return (
+      <div>
+      <form onSubmit={
+      handleSubmit
+      }
+
+      >
+      <input type="text" name="LINKDATA" placeholder="ADD NEW LINK TO SAVE" />
+      <button type="submit">ADD NEW LINK</button>
+      </form>
+      </div>)
+      }
+
+
+  export default addNewLink;
